0e02bcd6b59e5512d40303219553e6588f7b3790
Hanyu Wu <whanyu1212@hotmail.com>
2024-03-24 22:41:37 +0800

    main function

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ from loguru import logger
 from src.df_preprocessing import TextPreProcessing
 from src.dict_preprocessing import DictPreProcessing
 from src.sentiment_dict_approach import SentimentScoreDict
-from src.model_pipeline import ModelPipeline
+from src.lgbm_pipeline import ModelPipeline
 
 
 def load_files():
@@ -24,21 +24,29 @@ def main():
     logger.info(f"Starting the pipeline at {start_time}")
     try:
         zack_data, positive_words_df, negative_words_df, startup_data = load_files()
-        # text_processor = TextPreProcessing(data=zack_data, column="arguments_clean")
-        # dict_processor = DictPreProcessing(positive_words_df, negative_words_df)
-        # dict_sentiment = dict_processor.dict_processing_flow()
-        # zack_arguments_processed = text_processor.preprocessing_flow()
 
-        # startup_data_processed = text_processor.preprocessing_flow(
-        #     data=startup_data, column="Description"
-        # )
+        argument_text_processor = TextPreProcessing(
+            data=zack_data, column="arguments_clean"
+        )
+
+        dict_processor = DictPreProcessing(positive_words_df, negative_words_df)
+        processed_sentiment_dictionary = dict_processor.dict_processing_flow()
+        zack_arguments_processed = argument_text_processor.preprocessing_flow()
+        sentiment_calculator = SentimentScoreDict(
+            data=zack_arguments_processed,
+            col="Processed_arguments_clean_list",
+            dictionary=processed_sentiment_dictionary,
+        )
+        zack_arguments_w_sentiment = sentiment_calculator.sentiment_score_flow()
+
+        zack_arguments_w_sentiment.to_csv(
+            "./data/processed/zacks_arguments_sentiment.csv", index=False
+        )
+
         startup_text_processor = TextPreProcessing(
             data=startup_data, column="Description"
         )
         startup_data_processed = startup_text_processor.preprocessing_flow()
-        startup_data_processed["Processed_Description"] = startup_data_processed[
-            "Processed_Description"
-        ].apply(lambda x: " ".join(x))
 
         startup_data_processed.to_csv(
             "./data/processed/startup_data_processed.csv", index=False
@@ -46,26 +54,14 @@ def main():
 
         model_pipeline = ModelPipeline(
             data=startup_data_processed,
-            text_column="Processed_Description",
+            text_column="Processed_Description_str",
             label_column="Industry",
         )
 
         model_pipeline.modelling_flow()
 
-        # sentiment_calculator = SentimentScoreDict(
-        #     data=zack_arguments_processed,
-        #     col="processed_arguments",
-        #     dictionary=dict_sentiment,
-        # )
-
-        # zack_arguments_w_sentiment = sentiment_calculator.sentiment_score_flow()
-
         # logger.info(
         #     f"Processed zack's arguments: {zack_arguments_w_sentiment.head(10)}"
-        # )
-
-        # zack_arguments_w_sentiment.to_csv(
-        #     "./data/processed/zacks_arguments_sentiment.csv", index=False
         # )
 
         end_time = datetime.now()
